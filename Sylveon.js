0aaecb42bba924ea56ab817092a09ba4c7117fe3
Lily Hoopes <lilyhoopes@gmail.com>
2026-01-13 18:54:42 -0800

    added lily image to test how animation looks

--- Sylveon.js
+++ Sylveon.js
@@ -2,6 +2,8 @@ class Sylveon {
     constructor(game) {
         this.game = game;
         this.animator = new Animator(ASSET_MANAGER.getAsset("./sylveonSpriteSheet.png"), 0, 0, 63, 48, 4, .2);
+        //this.animator = new Animator(ASSET_MANAGER.getAsset("./LilySpriteSheet.png"), 0, 0, 246, 231, 4, .2);
+
     
         this.x = 0;
         this.y = 400;
--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./sylveonSpriteSheet.png")
+//ASSET_MANAGER.queueDownload("./LilySpriteSheet.png")
+
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
