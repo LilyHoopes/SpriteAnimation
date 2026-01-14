128d8184d2df7154746fd6f4e8f172dc848ebac3
Lily Hoopes <lilyhoopes@gmail.com>
2026-01-13 17:16:55 -0800

    added images and sizing, working project

---
+++ Sylveon.js
@@ -0,0 +1,20 @@
+class Sylveon {
+    constructor(game) {
+        this.game = game;
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./sylveonSpriteSheet.png"), 0, 0, 63, 48, 4, .2);
+    
+        this.x = 0;
+        this.y = 400;
+        this.speed = 85;
+    
+    };
+
+    update() {
+        this.x += this.speed * this.game.clockTick;
+        if(this.x > 512) this.x = 0;
+    };
+
+    draw(ctx) {
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    };
+}``
